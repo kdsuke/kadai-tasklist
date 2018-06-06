0fccd0eaeaef9b7c1a7798e42002fb0353e497e9
kdsuke <reidness1910@gmail.com>
2018-06-06 04:27:18 +0000

    user pages

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -14,7 +14,8 @@
                 <ul class="nav navbar-nav navbar-right">
                     
                      @if (Auth::check())
-                        <li><a href="#">Users</a></li>
+                        <li>{!! link_to_route('users.index', 'Users') !!}</li>
+                        
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                             <ul class="dropdown-menu">
